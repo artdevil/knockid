9f033728ef7f305564da2f85a74dab65ba65f94d
artdevil <artdvl91@gmail.com>
2012-11-11 00:44:55 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knockid::Application.config.secret_token = '2d6f2f7351030a7b9c8767bce25f27fa495c1be68603eff2100897eba2cdc3ddd1f4b3b7c85835a0772bd2227c09e64646fc7a0f87d4ad82e934cd2639e84dac'
