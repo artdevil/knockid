888940d2c58e2acd76fc9bb53cc83e164a3c9391
artdevil <artdvl91@gmail.com>
2012-11-23 00:14:35 +0700

    fix 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,19 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  def after_sign_up_path_for(resource)
+    edit_user_registration_path
+  end
+
   protected
 
   def layout_by_resource
-    if devise_controller?
+    if devise_controller? && action_name != 'edit'
       "devise_layout"
     else
       "application"
     end
   end
+
+  after
 end
