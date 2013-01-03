db63880af8ca7f9358617a2f09f04d2a43ac32ca
artdevil <artdvl91@gmail.com>
2013-01-03 19:46:19 +0700

    controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,6 +6,22 @@ class LocationsController < InheritedResources::Base
 	end
 
 	def new
-		@location = current_user.locations.new
+		if current_user.locations.length != 2
+			@location = current_user.locations.new
+		else
+			flash[:error] = "you only can have 2 location"
+			redirect_to locations_path
+		end
+	end
+
+	def create
+		@location = current_user.locations.new(params[:location])
+		if @location.save
+			flash[:success] = "your location has been created"
+			redirect_to locations_path
+		else
+			flash[:error] = "please review your input"
+			render 'new'
+		end
 	end
 end
